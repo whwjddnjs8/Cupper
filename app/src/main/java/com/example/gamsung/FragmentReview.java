1ff03a9c7b79dbcb6879b0b7a823372502f9624f
whwjddnjs8 <whwjddnjs8@naver,com>
2019-12-20 23:53:11 +0900

    Merge branch 'master' of https://github.com/rabbit9893/Gamsung
    # Conflicts:
    #	app/src/main/java/com/example/gamsung/CafeReview.java

--- app/src/main/java/com/example/gamsung/FragmentReview.java
+++ app/src/main/java/com/example/gamsung/FragmentReview.java
@@ -6,8 +6,10 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FragmentReview extends Fragment {
@@ -17,8 +19,11 @@ public class FragmentReview extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup rootview = (ViewGroup)inflater.inflate(R.layout.fragment_review, container, false);
-
-
+        recyclerView = (RecyclerView)rootview.findViewById(R.id.recycler_view);
+        reviewList = new ArrayList<>();
+        reviewAdapter = new ReviewAdapter(getActivity(), reviewList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(reviewAdapter);
         return rootview;
     }
 }
--- app/src/main/java/com/example/gamsung/ReviewAdapter.java
+++ app/src/main/java/com/example/gamsung/ReviewAdapter.java
@@ -1,8 +1,52 @@
 package com.example.gamsung;
 
-public class ReviewAdapter RecyclerView.Adapter<ReviewAdapter.myViewHolder>{
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ReviewAdapter extends RecyclerView.Adapter<ReviewAdapter.MyViewHolder> {
     private Context context;
-    private List<Circle> circleList;
+    private List<Review> reviewList;
+
+    public ReviewAdapter(Context mContext, List<Review> data) {
+        this.context = mContext;
+        reviewList = data;
+    }
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView username,cafename,rating,text,tag1,tag2,tag3;
+        public ImageView profile,revphoto;
+        public MyViewHolder(View view) {    // 뷰홀더가 만들어짐
+            super(view);
+            profile = view.findViewById(R.id.profile);
+            username = view.findViewById(R.id.username);
+            cafename = view.findViewById(R.id.cafename);
+            rating = view.findViewById(R.id.ratingbar);
+            text = view.findViewById(R.id.text);
+            revphoto = view.findViewById(R.id.reviewPhoto);
+            tag1 = view.findViewById(R.id.tag1);
+            tag2 = view.findViewById(R.id.tag2);
+            tag3 = view.findViewById(R.id.tag3);
+        }
+
+        public ReviewAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.review_content, parent, false);
+
+            return new ReviewAdapter.MyViewHolder(itemView);
+        }
+        public int getItemCount() {
+            return reviewList.size();
+        }
+    }
+}
 
 
 
